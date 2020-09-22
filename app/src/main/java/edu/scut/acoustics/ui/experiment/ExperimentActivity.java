ae5e238b33051118db770c9a4eff1739fe54d180
Coel <Coel@localhost>
2020-09-22 11:17:09 +0800

    工作：SplashActivity 和 ExperimentActivity

--- app/src/main/java/edu/scut/acoustics/ui/experiment/ExperimentActivity.java
+++ app/src/main/java/edu/scut/acoustics/ui/experiment/ExperimentActivity.java
@@ -1,6 +1,18 @@
 package edu.scut.acoustics.ui.experiment;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import edu.scut.acoustics.R;
+import edu.scut.acoustics.databinding.ActivityExperimentBinding;
 
 public class ExperimentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityExperimentBinding activityExperimentBinding = DataBindingUtil.setContentView(this, R.layout.activity_experiment);
+    }
 }
--- app/src/main/java/edu/scut/acoustics/ui/splash/SplashActivity.java
+++ app/src/main/java/edu/scut/acoustics/ui/splash/SplashActivity.java
@@ -1,14 +1,62 @@
 package edu.scut.acoustics.ui.splash;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
-public class SplashActivity extends AppCompatActivity {
+import edu.scut.acoustics.R;
+import edu.scut.acoustics.databinding.ActivitySplashBinding;
+import edu.scut.acoustics.ui.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener {
+    private Handler handler = new Handler();
+    private Runnable runnable = null;
+    private Button skip = null;
+    private CountDownTimer countDownTimer = new CountDownTimer(3200, 1000) {
+        @SuppressLint({"SetTextI18n", "DefaultLocale"})
+        @Override
+        public void onTick(long millisUntilFinished) {
+            skip.setText(getString(R.string.skip) + " " + String.format("%ds",millisUntilFinished/1000));
+        }
+
+        @SuppressLint({"SetTextI18n", "DefaultLocale"})
+        @Override
+        public void onFinish() {
+            skip.setText(getString(R.string.skip) + " " + String.format("%ds",0));
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivitySplashBinding activitySplashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        activitySplashBinding.skip.setOnClickListener(this);
+        skip = activitySplashBinding.skip;
 
+        handler.postDelayed(runnable = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },3200);
+        countDownTimer.start();
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+        if(handler != null && runnable != null){
+            handler.removeCallbacks(runnable);
+        }
     }
 }
