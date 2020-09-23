a36e5106ce5594feda269f9a658017148530048b
CoelisMyName <63242816+CoelisMyName@users.noreply.github.com>
2020-09-23 19:31:26 +0800

    增加一些东西

---
+++ app/src/test/java/edu/scut/acoustics/MirrorVector.java
@@ -0,0 +1,21 @@
+package edu.scut.acoustics;
+
+public class MirrorVector {
+
+    public class Token{
+    }
+
+    public class Alphabet extends Token{
+        public char a;
+    }
+
+    public class Dot extends Token{
+    }
+
+    public class Number extends Token{
+    }
+
+    public void scanner(){
+
+    }
+}
