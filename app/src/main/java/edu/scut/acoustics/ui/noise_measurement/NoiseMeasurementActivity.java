6927015c62d2f04ccc033ff184b547e688f051e5
CoelisMyName <63242816+CoelisMyName@users.noreply.github.com>
2020-09-25 19:51:03 +0800

    做声学实验功能

--- app/src/main/java/edu/scut/acoustics/ui/noise_measurement/NoiseMeasurementActivity.java
+++ app/src/main/java/edu/scut/acoustics/ui/noise_measurement/NoiseMeasurementActivity.java
@@ -1,5 +1,6 @@
 package edu.scut.acoustics.ui.noise_measurement;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,18 +12,35 @@ import java.util.Objects;
 
 import edu.scut.acoustics.R;
 import edu.scut.acoustics.databinding.ActivityNoiseMeasurementBinding;
+import edu.scut.acoustics.ui.adjust.AdjustActivity;
+import edu.scut.acoustics.ui.experiment.ExperimentActivity;
 
 public class NoiseMeasurementActivity extends AppCompatActivity implements View.OnClickListener {
+    private ActivityNoiseMeasurementBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityNoiseMeasurementBinding activity = DataBindingUtil.setContentView(this, R.layout.activity_noise_measurement);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_noise_measurement);
         Objects.requireNonNull(getSupportActionBar()).setHomeAsUpIndicator(R.drawable.ic_baseline_close_24);
     }
 
 
     @Override
     public void onClick(View view) {
+        if(view.getId() == binding.adjust.getId()){
+            start_adjust();
+        }
+        if(view.getId() == binding.refresh.getId()){
+            refresh();
+        }
+    }
+
+    public void start_adjust(){
+        startActivity(new Intent(this, AdjustActivity.class));
+    }
+
+    public void refresh(){
 
     }
+
 }
